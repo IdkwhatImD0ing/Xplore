c70e0fce3a6cfbb16f9551889b3fbf7348c28591
Restyled.io <commits@restyled.io>
2024-01-28 18:32:20 +0000

    Restyled by prettier

--- frontend/xplore/app/layout.js
+++ frontend/xplore/app/layout.js
@@ -1,33 +1,31 @@
 import "./globals.css";
 
-import {Box} from "@chakra-ui/react";
-import {ClerkProvider, UserButton} from "@clerk/nextjs";
-import {Inter} from "next/font/google";
+import { Box } from "@chakra-ui/react";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
 import Navbar from "./components/navbar";
 import Providers from "./providers";
 
-const inter = Inter({subsets : [ "latin" ]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title : "Xplore",
-  description : "The best travel companion ever!",
-  manifest : "/manifest.json",
+  title: "Xplore",
+  description: "The best travel companion ever!",
+  manifest: "/manifest.json",
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
-      <ClerkProvider><html lang = "en"><body className = {inter.className}>
-      <Providers>
-      <Navbar userButton =
-       {
-         <UserButton />
-       } />
-            <Box pt ="50px">
-              {children}
-            </Box>
-
-      </Providers>
-        </body></html>
-    </ClerkProvider>);
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Providers>
+            <Navbar userButton={<UserButton />} />
+            <Box pt="50px">{children}</Box>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
