1ee77f460155876aac8b1e548b98dd6bb91c49f7
Restyled.io <commits@restyled.io>
2024-01-28 02:33:46 +0000

    Restyled by prettier

--- frontend/xplore/app/page.js
+++ frontend/xplore/app/page.js
@@ -1,6 +1,6 @@
-import {ChakraProvider} from '@chakra-ui/react';
-import {UserButton} from "@clerk/nextjs";
-import {User} from "@clerk/nextjs/dist/types/server";
+import { ChakraProvider } from "@chakra-ui/react";
+import { UserButton } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server";
 import Image from "next/image";
 // export default function Home() {
 //   return (
@@ -19,14 +19,14 @@ import Image from "next/image";
 // }
 
 // src/App.js
-import React from 'react';
+import React from "react";
 
-import {AttractionResultsOutput} from "./components/attractionresultsoutput";
-import {AttractionTypeInput} from "./components/attractiontypeinput";
-import {CalculateRoutes} from "./components/calculateroutes";
-import {CityInput} from "./components/cityinput";
-import {DaySwitcher} from "./components/dayswitcher";
-import LandingPage from './components/landingpage';
+import { AttractionResultsOutput } from "./components/attractionresultsoutput";
+import { AttractionTypeInput } from "./components/attractiontypeinput";
+import { CalculateRoutes } from "./components/calculateroutes";
+import { CityInput } from "./components/cityinput";
+import { DaySwitcher } from "./components/dayswitcher";
+import LandingPage from "./components/landingpage";
 import styles from "./page.module.css";
 
 function App() {
--- frontend/xplore/app/sign-up/page.js
+++ frontend/xplore/app/sign-up/page.js
@@ -1,17 +1,17 @@
 // pages/sign-in.page.client.js (or wherever your sign-in page is located)
 "use client";
 
-import {SignUp} from "@clerk/nextjs";
-import {Container, VStack} from "@chakra-ui/react";
+import { SignUp } from "@clerk/nextjs";
+import { Container, VStack } from "@chakra-ui/react";
 
 export default function SignInPage() {
   return (
     <Container
-  minH = "100vh"
-  minW = "100vw"
-  display = "flex"
-  alignItems = "center"
-  justifyContent = "center"
+      minH="100vh"
+      minW="100vw"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
       bgGradient="linear(to-r, teal.200, blue.200)" // Example gradient background
     >
       <VStack spacing={8} w="full" maxW="md" px={6}>
