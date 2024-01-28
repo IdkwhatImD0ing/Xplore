d4c2146266496174109e2485596a2e9c67dd9646
eyang-zeta <eyang.zeta@gmail.com>
2024-01-27 18:29:44 -0800

    will it break this time

--- frontend/xplore/app/page.js
+++ frontend/xplore/app/page.js
@@ -1,23 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { CityInput } from "./components/cityinput";
-import { AttractionTypeInput } from "./components/attractiontypeinput";
-import { AttractionResultsOutput } from "./components/attractionresultsoutput";
-import { DaySwitcher } from "./components/dayswitcher";
-import { CalculateRoutes } from "./components/calculateroutes";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+// import { CityInput } from "./components/cityinput";
+// import { AttractionTypeInput } from "./components/attractiontypeinput";
+// import { AttractionResultsOutput } from "./components/attractionresultsoutput";
+// import { DaySwitcher } from "./components/dayswitcher";
+// import { CalculateRoutes } from "./components/calculateroutes";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <div id="leftcol">
+//         <CityInput />
+//         <AttractionTypeInput />
+//       </div>
+//       <div id="rightcol">
+//         <AttractionResultsOutput />
+//         <DaySwitcher />
+//         <CalculateRoutes />
+//       </div>
+//     </main>
+//   );
+// }
+
+// src/App.js
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import LandingPage from './components/landingpage';
+
+function App() {
   return (
-    <main className={styles.main}>
-      <div id="leftcol">
-        <CityInput />
-        <AttractionTypeInput />
-      </div>
-      <div id="rightcol">
-        <AttractionResultsOutput />
-        <DaySwitcher />
-        <CalculateRoutes />
-      </div>
-    </main>
+    <ChakraProvider>
+      <LandingPage />
+    </ChakraProvider>
   );
 }
+
+export default App;
