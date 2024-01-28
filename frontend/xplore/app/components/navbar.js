6dd8d064b4850dfd48f6b72f2fa535edc8c6385c
Restyled.io <commits@restyled.io>
2024-01-28 18:32:17 +0000

    Restyled by clang-format

--- frontend/xplore/app/components/navbar.js
+++ frontend/xplore/app/components/navbar.js
@@ -1,54 +1,43 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/navigation";
-import { Button, Image, Flex, Stack, Box } from "@chakra-ui/react";
-import { SignedOut, SignedIn, userButton } from "@clerk/nextjs";
+import {useRouter} from "next/navigation";
+import {Button, Image, Flex, Stack, Box} from "@chakra-ui/react";
+import {SignedOut, SignedIn, userButton} from "@clerk/nextjs";
 
 const Navbar = ({userButton}) => {
   const router = useRouter();
 
-  const handleSignInClick = () => {
-    router.push("/sign-in");
-  };
+  const handleSignInClick = () => { router.push("/sign-in"); };
 
-  const handleSignUpClick = () => {
-    router.push("/sign-up");
-  };
+  const handleSignUpClick = () => { router.push("/sign-up"); };
 
-  const handleLogoClick = () => {
-    router.push("/");
-  };
+  const handleLogoClick = () => { router.push("/"); };
 
-  const handleWizardClick = () => {
-    router.push("/wizard");
-  };
+  const handleWizardClick = () => { router.push("/wizard"); };
 
-  const handleMyRoutesClick = () => {
-    router.push("/inputRoute");
-  };
+  const handleMyRoutesClick = () => { router.push("/inputRoute"); };
 
   return (
     <Flex
       as="header"
-      align="center"
-      justify="space-between"
-      bg="purple.900"
-      p={3}
-      color="white"
-      position="absolute"
-      top="0"
-      left="0"
-      right="0"
-    >
-      <Image
-        src="/icons/icon-192x192.png"
-        alt="Company Logo"
-        maxW="65px"
-        height="50px"
-        cursor="pointer"
-        onClick={handleLogoClick}
-      />
+  align = "center"
+  justify = "space-between"
+  bg = "purple.900"
+  p = {3} color = "white"
+  position = "absolute"
+  top = "0"
+  left = "0"
+  right = "0" > < Image
+  src = "/icons/icon-192x192.png"
+  alt = "Company Logo"
+  maxW = "65px"
+  height = "50px"
+  cursor = "pointer"
+  onClick =
+  {
+    handleLogoClick
+  } />
       <Stack direction="row" spacing={4} align="center">
         <SignedOut>
           <Button
@@ -58,21 +47,20 @@ const Navbar = ({userButton}) => {
             onClick={handleSignInClick}
           >
             Sign In
-          </Button>
-          <Button
-            colorScheme="purple"
-            bg="purple.300"
-            _hover={{ bg: "purple.400" }}
-            onClick={handleSignUpClick}
-          >
-            Sign Up
-          </Button>
+          </Button >
+      < Button
+  colorScheme = "purple"
+  bg = "purple.300"
+  _hover =
+  {
+    { bg: "purple.400" }
+  } onClick = {handleSignUpClick} > Sign Up</Button>
         </SignedOut>
-        <SignedIn>
-          <Button
-            colorScheme="purple"
-            bg="purple.300"
-            _hover={{ bg: "purple.400" }}
+              <SignedIn>< Button
+  colorScheme = "purple"
+  bg = "purple.300"
+            _hover={
+    { bg: "purple.400" }}
             onClick={handleWizardClick}
           >
             Wizard
--- frontend/xplore/app/layout.js
+++ frontend/xplore/app/layout.js
@@ -1,34 +1,33 @@
 import "./globals.css";
 
-import { ClerkProvider, UserButton } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import {Box} from "@chakra-ui/react";
+import {ClerkProvider, UserButton} from "@clerk/nextjs";
+import {Inter} from "next/font/google";
 
 import Navbar from "./components/navbar";
 import Providers from "./providers";
-import { Box } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets : [ "latin" ]});
 
 export const metadata = {
-  title: "Xplore",
-  description: "The best travel companion ever!",
-  manifest: "/manifest.json",
+  title : "Xplore",
+  description : "The best travel companion ever!",
+  manifest : "/manifest.json",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <Providers>
-            <Navbar userButton={<UserButton />} />
+      <ClerkProvider><html lang = "en"><body className = {inter.className}>
+      <Providers>
+      <Navbar userButton =
+       {
+         <UserButton />
+       } />
             <Box pt ="50px">
               {children}
             </Box>
-            
-          </Providers>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+
+      </Providers>
+        </body></html>
+    </ClerkProvider>);
 }
