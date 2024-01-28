b26d2866ebe6ddfc2caa498842e205ea3d31b649
Restyled.io <commits@restyled.io>
2024-01-28 15:20:17 +0000

    Restyled by prettier

--- frontend/xplore/app/components/Layout.js
+++ frontend/xplore/app/components/Layout.js
@@ -1,10 +1,10 @@
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 const layoutVariants = {
-  initial : {opacity : 0},
-  animate : {opacity : 1},
-  exit : {opacity : 0},
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
 };
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <motion.div
       initial="initial"
