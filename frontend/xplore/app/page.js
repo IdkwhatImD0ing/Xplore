92d553d2e82a895728f21b1a8fdecae80b30ba80
eyang-zeta <eyang.zeta@gmail.com>
2024-01-28 04:33:23 -0800

    file organization

--- frontend/xplore/app/page.js
+++ frontend/xplore/app/page.js
@@ -1,6 +1,6 @@
 import {UserButton} from '@clerk/nextjs'
 import React from 'react'
-import LandingPage from './components/landingpage'
+import LandingPage from './landingpage/landingpage'
 
 function App() {
   return (
