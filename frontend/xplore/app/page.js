b80097cd84de88cfd51116c5edfc8108e57a54c7
Restyled.io <commits@restyled.io>
2024-01-28 02:33:43 +0000

    Restyled by clang-format

--- frontend/xplore/app/page.js
+++ frontend/xplore/app/page.js
@@ -1,12 +1,7 @@
+import {ChakraProvider} from '@chakra-ui/react';
+import {UserButton} from "@clerk/nextjs";
+import {User} from "@clerk/nextjs/dist/types/server";
 import Image from "next/image";
-import styles from "./page.module.css";
-import { CityInput } from "./components/cityinput";
-import { AttractionTypeInput } from "./components/attractiontypeinput";
-import { AttractionResultsOutput } from "./components/attractionresultsoutput";
-import { DaySwitcher } from "./components/dayswitcher";
-import { CalculateRoutes } from "./components/calculateroutes";
-import { UserButton } from "@clerk/nextjs";
-
 // export default function Home() {
 //   return (
 //     <main className={styles.main}>
@@ -25,9 +20,14 @@ import { UserButton } from "@clerk/nextjs";
 
 // src/App.js
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+
+import {AttractionResultsOutput} from "./components/attractionresultsoutput";
+import {AttractionTypeInput} from "./components/attractiontypeinput";
+import {CalculateRoutes} from "./components/calculateroutes";
+import {CityInput} from "./components/cityinput";
+import {DaySwitcher} from "./components/dayswitcher";
 import LandingPage from './components/landingpage';
-import { User } from "@clerk/nextjs/dist/types/server";
+import styles from "./page.module.css";
 
 function App() {
   return (
